923cc54639a17dbcdcb175a0d0242de5032b7b92
Lucas Falkenstein <lfalkenstein9@gmail.com>
2017-02-19 17:47:00 -0500

    Ocaml layer for emacs

--- emacs/.emacs.d/files/setup-ocaml.el
+++ emacs/.emacs.d/files/setup-ocaml.el
@@ -1,13 +1,24 @@
 (require 'packaging)
+(require-package 'smartparens)
 ;; (require-package 'ocp-indent)
 (require-package 'tuareg)
+;; Merlin was installed with OPAM
 ;; (require-package 'company) ;; this is likely covered by merlin
 ;; TODO get automformat func, is it in ocp-indent?
-;; 
 
 (setq tuareg-match-patterns-aligned t)
-;; (add-hook 'tuareg-mode-hook 'merlin-mode)
-;; (add-to-list 'load-path (expand-file-name "~/.opam/system/share/emacs/site-lisp"))
+
+
+(let ((opam-share (ignore-errors (car (process-lines "opam" "config" "var" "share")))))
+  (when (and opam-share (file-directory-p opam-share))
+    (add-to-list 'load-path (expand-file-name "emacs/site-lisp" opam-share))
+    (autoload 'merlin-mode "merlin" nil t nil)
+    (add-hook 'tuareg-mode-hook 'merlin-mode t)
+    (setq merlin-command "ocamlmerlin")))
+
+(add-hook 'tuareg-mode-hook 'smartparens-mode)
+
+(provide 'setup-ocaml)))
 
 
 (provide 'setup-ocaml)

4d0a0744ae64e6ffb176ad06b2e3d7a40058fb4c
Lucas Falkenstein <lfalkenstein9@gmail.com>
2017-02-22 18:55:55 -0500

    expanded ocaml layer, still need to get company/merlin complete working though, and maybe utop integration

--- emacs/.emacs.d/files/setup-ocaml.el
+++ emacs/.emacs.d/files/setup-ocaml.el
@@ -1,25 +1,27 @@
+;; TODO this is kinda hacky and relies on some cp'd stuff. Clean it up
+
 (require 'packaging)
 (require-package 'smartparens)
 ;; (require-package 'ocp-indent)
 (require-package 'tuareg)
 ;; Merlin was installed with OPAM
-(require-package 'company) ;; this is likely covered by merlin
-;; TODO get automformat func, is it in ocp-indent?
+(require-package 'company)
+;; TODO get automformat func, is it in ocp-indent?;;
+(require-package 'utop)
 
 (setq tuareg-match-patterns-aligned t)
-
 
 (let ((opam-share (ignore-errors (car (process-lines "opam" "config" "var" "share")))))
   (when (and opam-share (file-directory-p opam-share))
     (add-to-list 'load-path (expand-file-name "emacs/site-lisp" opam-share))
     (autoload 'merlin-mode "merlin" nil t nil)
-    (add-hook 'tuareg-mode-hook 'merlin-mode t)
-    (with-eval-after-load 'company
-      (add-to-list 'company-backends 'merlin-company-backend))
-    (add-hook 'merlin-mode-hook 'merlin-mode-hook)
-    (setq merlin-command "ocamlmerlin")))
+    (add-hook 'tuareg-mode-hook 'merlin-mode t)))
+
+(with-eval-after-load 'company
+  (add-to-list 'company-backends 'merlin-company-backend))
 
 (add-hook 'tuareg-mode-hook 'smartparens-mode)
 
+(add-hook 'merlin-mode-hook 'company-mode)
 
 (provide 'setup-ocaml)

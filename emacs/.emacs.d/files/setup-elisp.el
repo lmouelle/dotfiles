45dfda80f747fdbf2a2513623e56e9584bf1ac44
Lucas Falkenstein <lfalkenstein9@gmail.com>
2017-02-27 09:49:22 -0500

    elisp file cleanup

--- emacs/.emacs.d/files/setup-elisp.el
+++ emacs/.emacs.d/files/setup-elisp.el
@@ -1,22 +1,15 @@
 (require 'packaging)
 
 (require-package 'auto-compile)
-(require 'auto-compile)
-
 (require-package 'company)
-(require 'company)
-
 (require-package 'paredit)
-(require 'paredit)
-
 (require-package 'rainbow-delimiters)
-(require 'rainbow-delimiters)
 
 (add-hook 'emacs-lisp-mode-hook #'company-mode)
 
-(add-hook 'emacs-lisp-mode-hook #'enable-paredit-mode)
-(add-hook 'eval-expression-minibuffer-setup-hook #'enable-paredit-mode)
-(add-hook 'lisp-interaction-mode-hook #'enable-paredit-mode)
+(add-hook 'emacs-lisp-mode-hook #'paredit-mode)
+(add-hook 'eval-expression-minibuffer-setup-hook #'paredit-mode)
+(add-hook 'lisp-interaction-mode-hook #'paredit-mode)
 (add-hook 'lisp-mode-hook #'enable-paredit-mode)
 
 (add-hook 'emacs-lisp-mode-hook #'turn-on-eldoc-mode)
@@ -26,4 +19,3 @@
 (add-hook 'lisp-mode-hook #'rainbow-delimiters-mode)
 
 (provide 'setup-elisp)
-

59fcb6c01e55eab5d1f12b610801558c4db6b506
Lucas Falkenstein <louellet@redhat.com>
2017-06-21 11:29:43 -0400

    Added comment to clarify confusing behaviour in elisp layer with rainbow parens

--- emacs/.emacs.d/files/setup-elisp.el
+++ emacs/.emacs.d/files/setup-elisp.el
@@ -15,6 +15,7 @@
 (add-hook 'emacs-lisp-mode-hook #'turn-on-eldoc-mode)
 (add-hook 'lisp-interaction-mode-hook #'turn-on-eldoc-mode)
 
+;; This mode does not show in the mode list bar, don't be surprised!
 (add-hook 'emacs-lisp-mode-hook #'rainbow-delimiters-mode)
 (add-hook 'lisp-mode-hook #'rainbow-delimiters-mode)
 

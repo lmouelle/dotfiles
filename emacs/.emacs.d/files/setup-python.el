70dd2b9c38237d08cfe9a2010ba9c6801ff4f4ca
lu <lu@charon.umd.edu>
2017-09-19 13:05:33 -0400

    Removed ipython shell usage from python layer.
    Doesn't work consistently and frankly it's not a needed feature for me
    right now

--- emacs/.emacs.d/files/setup-python.el
+++ emacs/.emacs.d/files/setup-python.el
@@ -16,7 +16,4 @@
 (eval-after-load "company"
   '(add-to-list 'company-backends '(company-anaconda :with company-capf)))
 
-(when (executable-find "ipython")
-  (setq python-shell-interpreter "ipython"))
-
 (provide 'setup-python)

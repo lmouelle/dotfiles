0e22c7f52b4399f91773d844b7a39961d91dad7d
Lucas Ouellette-Falkenstein <lmouelle@users.noreply.github.com>
2021-09-25 09:30:32 -0700

    Zero fish greeting and ssh-eval

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -7,3 +7,11 @@ set -gx EDITOR "emacsclient -t"
 
 # OPAM configuration
 source /home/$USER/.opam/opam-init/init.fish > /dev/null 2> /dev/null; or true
+
+# The $fish_greeting breaks certain applications like Winscp
+# so zero it out to nothing
+set fish_greeting
+
+# Start ssh agent so we can list/manage keys, and output csh-style
+# syntax for setting env variables that are friendly to fish
+eval (ssh-agent -c)
